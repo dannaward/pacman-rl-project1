1aa88350c66c7fb40f0d28b6c2365a42cba1b59e
Danna <danna.wa@kakaobrain.com>
2022-10-09 00:47:08 +0900

    refactor: write dfs again with pseudocode

--- search.py
+++ search.py
@@ -115,54 +115,28 @@ def depthFirstSearch(problem):
     print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
     """
     "*** YOUR CODE HERE ***"
-    from util import Stack
+    start_node = problem.getStartState()
 
-    # stackXY: ((x,y),[path]) #
-    stackXY = Stack()
-
-    visited = []  # Visited states
-    path = []  # Every state keeps it's path from the starting state
-
-    # Check if initial state is goal state #
-    if problem.isGoalState(problem.getStartState()):
+    is_start_a_goal = problem.isGoalState(start_node)
+    if is_start_a_goal:
         return []
 
-    # Start from the beginning and find a solution, path is an empty list #
-    stackXY.push((problem.getStartState(), []))
+    stack = util.Stack()
+    visited = []
 
-    while (True):
+    stack.push((start_node, []))
 
-        # Terminate condition: can't find solution #
-        if stackXY.isEmpty():
-            return []
+    while not stack.isEmpty():
+        node, actions = stack.pop()
+        visited.append(node)
 
-        # Get informations of current state #
-        xy, path = stackXY.pop()  # Take position and path
-        visited.append(xy)
+        if problem.isGoalState(node):
+            return actions
 
-        # Comment this and uncomment 125. This only works for autograder    #
-        # In lectures we check if a state is a goal when we find successors #
-
-        # Terminate condition: reach goal #
-        if problem.isGoalState(xy):
-            return path
-
-        # Get successors of current state #
-        succ = problem.expand(xy)
-
-        # Add new states in stack and fix their path #
-        if succ:
-            for item in succ:
-                if item[0] not in visited:
-                    # Lectures code:
-                    # All impementations run in autograder and in comments i write
-                    # the proper code that i have been taught in lectures
-                    # if item[0] not in visited and item[0] not in (state[0] for state in stackXY.list):
-                    #   if problem.isGoalState(item[0]):
-                    #       return path + [item[1]]
-
-                    newPath = path + [item[1]]  # Calculate new path
-                    stackXY.push((item[0], newPath))
+        for next_node, action, cost in problem.expand(node):
+            if next_node not in visited:
+                next_action = actions + [action]
+                stack.push((next_node, next_action))
 
 
 def breadthFirstSearch(problem):
